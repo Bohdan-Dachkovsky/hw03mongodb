2373867407bdc362324fbbdc689580d23fdcf3d2
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2023-04-05 22:21:33 +0300

    Update contactsMiddlware.js

--- middlewares/contactsMiddlware.js
+++ middlewares/contactsMiddlware.js
@@ -44,10 +44,10 @@ exports.dltArray = catchAuthErr(async (req, res, next) => {
 exports.checkCreateData = catchAuthErr(async (req, res, next) => {
   const { error, value } = createUserDataValidator(req.body)
   if (error) return next(new AppErr())
-  const userExist = await User.findOne({ name: value.name })
+  // const userExist = await User.findOne({ name: value.name })
 
-  console.log(userExist)
-  req.body = value
+  // console.log(userExist)
+  // req.body = value
 
   next()
 })
