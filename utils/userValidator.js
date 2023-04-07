72229397d5a512a8a7478acef59812e414d0a13b
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2023-04-08 00:23:29 +0300

    Delete regex for number
    RegExp function

--- utils/userValidator.js
+++ utils/userValidator.js
@@ -1,16 +1,11 @@
 const Joi = require('joi')
-const PASSWORD_RULE =
-  '(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[$@$!#.])[A-Za-zd$@$!%*?&.]{8,20}'
+// const PASSWORD_RULE = '^[0-9+]{7}-[0-9+]{1}$'
 exports.createUserDataValidator = (data) =>
   Joi.object()
     .keys({
       name: Joi.string().max(15).required(),
       email: Joi.string().max(35).required(),
-      phone: Joi.string()
-        .min(9)
-        .max(13)
-        .regex(RegExp(PASSWORD_RULE))
-        .required(),
+      phone: Joi.string().min(9).max(13).required(),
       favorite: Joi.boolean,
     })
     .validate(data)
