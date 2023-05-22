6dbd074d7f067e0332c403b72cf69ae731039700
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2023-05-22 20:12:22 +0300

    Added limits parameters

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 const express = require('express')
 const path = require('path')
+// const fs = require('fs').promises
 const app = express()
 const multer = require('multer')
 require('dotenv').config({ path: './.env' })
 app.use(express.urlencoded({ extended: false }))
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
 app.use(cookieParser())
-const uploadDir = path.join(process.cwd(), 'temp')
+const uploadDir = path.join(__dirname, 'temp')
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, uploadDir)
       },
       filename: (req, file, cb) => {
-        cb(null, file.originalname)
+        cb(null, file.gitpicture)
       },
       limits: {
-        fileSize: 1048576,
+        fileSize: 9500000,
       },
 })
 const upload = multer({
@@ -27,10 +28,10 @@ const upload = multer({
   })
 const files = []
 app.get('/avatars', (req, res) => {
-res.json({files})
+res.json(files)
 }) 
-app.post('/avatars', upload.single('foto'), (res, req) => {
-console.log(req.file)
+app.post('/avatars', upload.single('photo'), (res, req) => {
+// await fs.rename()
 }) 
 app.listen(process.env.PORT, () => {
 console.log('File is running with multer')
